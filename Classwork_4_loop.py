22e3cb3314c77a4799bcb7c6fd6bd607c15c6bb7
Sona-py <62652056+Sona-py@users.noreply.github.com>
2020-04-10 02:49:29 -0700

    Delete Classwork_4_loop.py

--- Classwork_4_loop.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    fac = 1
-    for i in range(1, n + 1):
-        fac = fac * i
-    return fac
-if __name__ == "__main__":
-    n=int(input("Enter the number"))
-    print(factorial(n))
