731d63744ac8d35755ba462feacc4d58542ce74d
Sona-py <62652056+Sona-py@users.noreply.github.com>
2020-04-14 07:26:11 -0700

    Add files via upload

---
+++ 14.04.20_Homework/6. Homework_revised(1).py
@@ -0,0 +1,11 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+big = max(a, b)
+small = min(a, b)
+def divider(big,small):
+    mult = (big - big % small)//small
+    if big%small==0:
+        return small
+    return divider(small,big%small)
+print(divider(big,small))
+
