a4502829f85ea102f836834a051e8deea71c8f29
Sona-py <62652056+Sona-py@users.noreply.github.com>
2020-04-19 11:03:30 -0700

    Update homework1.py

--- week1/homework1.py
+++ week1/homework1.py
@@ -15,3 +15,21 @@ discriminant = lambda a, b, c : b**2-4*a*c
 full_name=lambda x,y: x+' '+y
 
 
+def mirror_string(a:str):
+    n=list(a)
+    b=[]
+    s=''
+    for i in n:
+        if 65<=ord(i)<= 90:
+            b.append(chr(90-(26-(90-ord(i))-1)))
+
+        elif 97<=ord(i)<=122:
+            b.append(chr(122-(26-(122-ord(i))-1)))
+
+        else:
+            b.append(i)
+
+    return s.join(b)
+
+
+
