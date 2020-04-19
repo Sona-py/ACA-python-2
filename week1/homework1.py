d6b017fc44798c31a8f4644f9a51a8f783f2ab03
Sona-py <62652056+Sona-py@users.noreply.github.com>
2020-04-19 07:03:56 -0700

    Update homework1.py

--- week1/homework1.py
+++ week1/homework1.py
@@ -2,7 +2,7 @@ def all_positive(k,*args):
     return sum((abs(x) for x in args),abs(k))>sum(args,k)
 
 def xor3(a,b,c):
-    return not((a and b and c) or not(a or b or c))
+    return bool((a and b and c) or (a and not(b) and not(c)) or (b and not(a) and not(c)) or (c and not(a) and not(b)))
 
 def binary_sum(x:str,y:str):
     return int(x,2)+int(y,2)
