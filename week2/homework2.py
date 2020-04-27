449df505f20a575d6ad90495001d1b54512ac0b1
Sona-py <62652056+Sona-py@users.noreply.github.com>
2020-04-27 00:56:02 -0700

    Update homework2.py

--- week2/homework2.py
+++ week2/homework2.py
@@ -62,3 +62,17 @@ def duplicate_characters(s):
     if len(dub)==0:
         return {}
     return dub
+
+
+def bisect_position(a, x, lo=0, hi=None):
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if x < a[mid]:
+            hi = mid
+        else:
+            lo = mid+1
+    return lo
