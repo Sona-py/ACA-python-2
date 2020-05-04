22e701f6b7170aa0f43034792fe9299265907bcf
Sona-py <62652056+Sona-py@users.noreply.github.com>
2020-05-04 01:34:24 -0700

    Add files via upload

---
+++ week3/homework3.py
@@ -0,0 +1,15 @@
+import numpy as np
+def arr_replace(x):
+    x=np.array(x)
+    x[x%2!=0]=0
+    return x
+
+import numpy as np
+def arr_replace_where(x):
+    x = np.array(x)
+    return np.where(x%2==0,0,x)
+
+import numpy as np
+def arr_repeat(x):
+    x = np.array(x)
+    return np.repeat(x,3,0)
